686d5e4c4f611f1e589ba0b7fcf0554dfbdf7d0d
LotteWac <lotte_wej@mz.co.kr>
2019-03-07 11:57:35 +0900

    190307
    commit

--- js/common.js
+++ js/common.js
@@ -8,6 +8,30 @@ $(document).ready(function(){
     });
 
     $('[data-toggle="tooltip"]').tooltip();
+
+
+    var num = 0, $nowActiveEL, $textOfEL;
+    var log = function(){
+        num  = num + 1;
+        $nowActiveEL = $(document.activeElement);
+        $textOfEL = $nowActiveEL.text();
+        console.log($nowActiveEL,num,$textOfEL);
+        return num;
+    }
+    var focusinHandler = function(){
+        $nowActiveEL = $(document.activeElement);
+        $nowActiveEL.css({
+            'outline-offset' : '-5px;',
+            'outline' : '1px solid red'
+        });
+        log ();
+    }
+    var focusoutHandler = function(){
+        $(this).css('outline', 'none');
+    }
+
+    $(document).on({focusin : focusinHandler});
+    $(document).on({focusout : focusoutHandler}, 'a:visible, button:visible, :input:not(:hidden), [tabindex], [href]');
 });
 
 
