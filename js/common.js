97e49e94970b66bca68bd9a15c7408e6f3641cbe
LotteWac <lotte_wej@mz.co.kr>
2019-03-13 09:32:28 +0900

    190313
    commit

--- js/common.js
+++ js/common.js
@@ -10,29 +10,29 @@ $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
 
 
-    var num = 0, $nowActiveEL, $textOfEL;
-    var log = function(){
-        num  = num + 1;
-        $nowActiveEL = $(document.activeElement);
-        $textOfEL = $nowActiveEL.text();
-        console.log($nowActiveEL,num,$textOfEL);
-        return num;
-    }
-    var focusinHandler = function(){
-        $nowActiveEL = $(document.activeElement);
-        // $nowActiveEL.css({
-        //     'outline-offset' : '-5px;',
-        //     'outline' : '1px solid red'
-        // });
-        $nowActiveEL.attr('style', 'outline:1px solid red; outline-offset:-1px;')
-        log ();
-    }
-    var focusoutHandler = function(){
-        $(this).css('outline', 'none').css('outline-offset', '0');
-    }
+    // var num = 0, $nowActiveEL, $textOfEL;
+    // var log = function(){
+    //     num  = num + 1;
+    //     $nowActiveEL = $(document.activeElement);
+    //     $textOfEL = $nowActiveEL.text();
+    //     console.log($nowActiveEL,num,$textOfEL);
+    //     return num;
+    // }
+    // var focusinHandler = function(){
+    //     $nowActiveEL = $(document.activeElement);
+    //     // $nowActiveEL.css({
+    //     //     'outline-offset' : '-5px;',
+    //     //     'outline' : '1px solid red'
+    //     // });
+    //     $nowActiveEL.attr('style', 'outline:1px solid red; outline-offset:-1px;')
+    //     log ();
+    // }
+    // var focusoutHandler = function(){
+    //     $(this).css('outline', 'none').css('outline-offset', '0');
+    // }
 
-    $(document).on({focusin : focusinHandler});
-    $(document).on({focusout : focusoutHandler}, 'a:visible, button:visible, :input:not(:hidden), [tabindex], [href]');
+    // $(document).on({focusin : focusinHandler});
+    // $(document).on({focusout : focusoutHandler}, 'a:visible, button:visible, :input:not(:hidden), [tabindex], [href]');
 });
 
 
